5e161b9d325de177d8dbb3a190b02baa390998c5
Kathembe <kimkathembe@gmail.com>
2017-02-27 14:53:41 +0300

    final

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source('https://rubygems.org')
 
-  gem('sinatra')
-  gem('sinatra-contrib')
-  gem('rspec')
-  gem('capybara')
-  gem('pry')
+gem('sinatra')
+gem('sinatra-contrib')
+gem('rspec')
+gem('capybara')
+gem('pry')
--- app.rb
+++ app.rb
@@ -1,56 +1,36 @@
 require('sinatra')
-  require('sinatra/reloader')
-  require('./lib/lend')
-  require('./lib/user')
-  also_reload('lib/**/*.rb')
+require('sinatra/reloader')
+require('./lib/loans_calculator')
+also_reload('lib/**/*.rb')
 
-  get('/') do
-    erb(:index)
-  end
 
-  get('/users/new') do
-    erb(:new_account)
-  end
+get('/') do
+  erb(:index)
+end
 
-  get('/users') do
-     @users = Account.all()
-     erb(:users)
-  end
 
-  post('/users') do
-    @name = params.fetch('name')
-    Account.new(@name).save()
-    erb(:submission)
-  end
+get('/loan') do
+  erb(:lend_form)
+end
 
-  get('/users/:id') do
-    @user = Account.find(params.fetch('id').to_i())
-    erb(:user)
-  end
+get('/borrow') do
+  erb(:borrow_form)
+end
 
-  get('/users/:id/request/new') do
-      @user = Account.find(params.fetch('id').to_i())
-      erb(:req_loan_form)
-  end
+post('/loan') do
+  @name = params.fetch('name')
+  @amount = params.fetch('amount').to_i()
+  @loan_apply = Loan.new(@name, @amount).loans_calculator()
+  loans = Loan.new(@name, @loan_apply)
+  loans.save()
+  erb(:list_full)
+end
 
-  get('/users/:id/offer/new') do
-      @user = Account.find(params.fetch('id').to_i())
-      erb(:lend_form)
-  end
-
-  get('/loan_detail') do
-    @name = params.fetch("person")
-     @amount = params.fetch("amnt").to_i
-     @month = params.fetch("months")
-     loaning= Loan.new(@name,@amount,@month)
-     loaning.save()
-     @user = Account.find(params.fetch('user_id').to_i())
-    # @user.add_loans(loaning)
-    # @int = @user.borrow()
-     erb(:accounts)
-  end
-
-  get('/my-account/:id') do
-    @loans = Loan.find(params.fetch("id"))
-    erb(:loan_detail)
-  end
+post('/borrow') do
+  @name = params.fetch('name')
+  @amount = params.fetch('amount').to_i()
+  @loan_apply = Loan.new(@name, @amount).loans_calculator()
+  loans = Loan.new(@name, @loan_apply)
+  loans.save()
+  erb(:list_debt)
+end
